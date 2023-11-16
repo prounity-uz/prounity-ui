f676edc496b4c99ca66e1ccea0fd5d5965554ef3
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2023-11-16 14:09:24 +0500

    Deleted

--- packages/react/dist/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./components/react-component-lib";
-export { defineCustomElements } from "@prounity/ui/loader";
-//# sourceMappingURL=index.js.map
--- packages/react/dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./components/react-component-lib";
-export { defineCustomElements } from "@prounity/ui/loader";
