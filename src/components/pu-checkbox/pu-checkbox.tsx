fc0eb440cd5a4aed36d091ad121980d5311a32e5
amirbek <amirbekazimov7@gmail.com>
2023-11-20 18:28:17 +0500

    checkbox with div

--- src/components/pu-checkbox/pu-checkbox.tsx
+++ src/components/pu-checkbox/pu-checkbox.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, Prop, h } from '@stencil/core';
+import { Component, Host, Prop, h, State, Watch, Element } from '@stencil/core';
 
 @Component({
   tag: 'pu-checkbox',
@@ -6,19 +6,47 @@ import { Component, Host, Prop, h } from '@stencil/core';
   shadow: true,
 })
 export class PuCheckbox {
+  @State() private checkboxInput: HTMLInputElement;
+  @State() private showPuIcon: boolean = false; // Новое состояние для отображения/скрытия pu-icon
   @Prop() checked: boolean;
   @Prop() disabled: boolean = false;
-  @Prop() hasError: boolean = false;
+  @Prop() hasError: boolean = true;
+
+  @Watch('checked')
+  handleCheckedChange() {
+    this.showPuIcon = this.checked; // Обновлено состояние showPuIcon при изменении checked
+  }
+
+  connectedCallback() {
+    this.handleInputClick = this.handleInputClick.bind(this);
+  }
+
+  componentDidLoad() {
+    if (this.checkboxInput) {
+      this.checkboxInput.addEventListener('change', this.handleCheckedChange);
+    }
+  }
+
+  private handleInputClick() {
+    if (!this.disabled) {
+      this.checked = !this.checked;
+    }
+  }
 
   render() {
-    console.log(this.hasError);
-    console.log('hello world');
-
     return (
       <Host>
-        <slot>
-          <input disabled={this.disabled} class={`pu-checkbox ${this.hasError ? 'error' : ''}`} type="checkbox" />
-        </slot>
+        <div class={`pu-checkbox ${this.hasError ? 'error' : ''}`}>
+          <pu-icon style={{ display: this.showPuIcon ? 'block' : 'none' }}>check</pu-icon>
+          <input
+            ref={el => (this.checkboxInput = el)}
+            disabled={this.disabled}
+            class={`pu-checkbox-input`}
+            type="checkbox"
+            checked={this.checked}
+            onClick={this.handleInputClick}
+          />
+        </div>
       </Host>
     );
   }
