e57c8f902566613c34e9143cbe7b834c12cb18be
amirbek <amirbekazimov7@gmail.com>
2023-11-21 11:09:36 +0500

    style

--- src/components/pu-checkbox/pu-checkbox.tsx
+++ src/components/pu-checkbox/pu-checkbox.tsx
@@ -1,4 +1,52 @@
-import { Component, Host, Prop, h, State, Watch, Element } from '@stencil/core';
+// import { Component, Host, Prop, h, State, Watch, Element } from '@stencil/core';
+
+// @Component({
+//   tag: 'pu-checkbox',
+//   styleUrl: 'pu-checkbox.scss',
+//   shadow: true,
+// })
+// export class PuCheckbox {
+//   @State() private checkboxInput: HTMLInputElement;
+//   @State() private showPuIcon: boolean = false;
+//   @Prop() checked: boolean;
+//   @Prop() disabled: boolean = false;
+//   @Prop() hasError: boolean = false;
+
+//   @Watch('checked')
+//   handleCheckedChange() {
+//     this.showPuIcon = this.checked;
+//   }
+
+//   connectedCallback() {
+//     this.handleInputClick = this.handleInputClick.bind(this);
+//   }
+
+//   private handleInputClick() {
+//     if (!this.disabled) {
+//       this.checked = !this.checked;
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <Host>
+//         <div class={`pu-checkbox ${this.hasError ? 'error' : ''}`}>
+//           <pu-icon style={{ display: this.showPuIcon ? 'block' : 'none' }}>check</pu-icon>
+//           <input
+//             ref={el => (this.checkboxInput = el)}
+//             disabled={this.disabled}
+//             class={`pu-checkbox-input`}
+//             type="checkbox"
+//             checked={this.checked}
+//             onClick={this.handleInputClick}
+//           />
+//         </div>
+//       </Host>
+//     );
+//   }
+// }
+
+import { Component, Host, Prop, h, State, Watch } from '@stencil/core';
 
 @Component({
   tag: 'pu-checkbox',
@@ -6,24 +54,27 @@ import { Component, Host, Prop, h, State, Watch, Element } from '@stencil/core';
   shadow: true,
 })
 export class PuCheckbox {
-  @State() private checkboxInput: HTMLInputElement;
-  @State() private showPuIcon: boolean = false;
-  @Prop() checked: boolean;
+  @State() private puIcon: string | null = null;
   @Prop() disabled: boolean = false;
   @Prop() hasError: boolean = false;
-
-  @Watch('checked')
-  handleCheckedChange() {
-    this.showPuIcon = this.checked;
-  }
 
   connectedCallback() {
     this.handleInputClick = this.handleInputClick.bind(this);
   }
 
+  private updateIcon() {
+    if (this.puIcon === null) {
+      this.puIcon = 'check';
+    } else if (this.puIcon === 'check') {
+      this.puIcon = 'horizontal_rule';
+    } else {
+      this.puIcon = null;
+    }
+  }
+
   private handleInputClick() {
     if (!this.disabled) {
-      this.checked = !this.checked;
+      this.updateIcon();
     }
   }
 
@@ -31,15 +82,8 @@ export class PuCheckbox {
     return (
       <Host>
         <div class={`pu-checkbox ${this.hasError ? 'error' : ''}`}>
-          <pu-icon style={{ display: this.showPuIcon ? 'block' : 'none' }}>check</pu-icon>
-          <input
-            ref={el => (this.checkboxInput = el)}
-            disabled={this.disabled}
-            class={`pu-checkbox-input`}
-            type="checkbox"
-            checked={this.checked}
-            onClick={this.handleInputClick}
-          />
+          {this.puIcon !== null && <pu-icon style={{ display: this.puIcon === 'check' || this.puIcon === 'horizontal_rule' ? 'block' : 'none' }}>{this.puIcon}</pu-icon>}
+          <input disabled={this.disabled} class={`pu-checkbox-input`} type="checkbox" onClick={this.handleInputClick} />
         </div>
       </Host>
     );
